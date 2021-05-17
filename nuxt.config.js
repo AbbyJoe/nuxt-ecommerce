8062cba7af15a3312ce4783e88aca0b9a960f976
Arigbede Abiodun <arigbedeabiodun@gmail.com>
2021-05-17 15:48:45 +0100

    add name

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'e-commerce',
+    title: 'cassiopeia',
     htmlAttrs: {
       lang: 'en'
     },
