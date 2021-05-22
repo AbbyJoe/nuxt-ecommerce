f047e7ffd86119c12209f8271fcf19f9e6aed593
Arigbede Abiodun <arigbedeabiodun@gmail.com>
2021-05-22 23:00:58 +0100

    add made by in footer

--- components/UI/appFooter.vue
+++ components/UI/appFooter.vue
@@ -31,6 +31,7 @@
                 <a href="https://twitter.com/Abby_joeRGX"><img src="../../assets/twitter.svg" class="inline w-auto h-8" alt=""></a>
                 <a href="https://www.instagram.com/abbyjoe___/"><img src="../../assets/instagram.svg" class="inline w-auto h-8" alt=""></a>
             </div>
+            <p class="pt-3 text-center">Made with ❤️ Abiodun Arigbede</p>
         </div>
     </section>
 </template>
